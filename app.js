170c7a26e616fc107cfd4cb32d37cc280683aba7
marc3601 <62243477+marc3601@users.noreply.github.com>
2020-05-23 22:50:50 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,128 @@
+class Timer {
+    constructor() {
+        this.hr = 0
+        this.min = 0
+        this.sec = 0
+        this.secCount
+        this.minCount
+        this.hrCount
+
+        this.Selectors = {
+            hours: '[data-hours]',
+            minutes: '[data-minutes]',
+            seconds: '[data-seconds]',
+            button: '[data-button]',
+            buttonStop: '[data-stopbtn]'
+        }
+    }
+
+    initializeTimer() {
+        this.hours = document.querySelector(this.Selectors.hours)
+        this.minutes = document.querySelector(this.Selectors.minutes)
+        this.seconds = document.querySelector(this.Selectors.seconds)
+        this.button = document.querySelector(this.Selectors.button)
+        this.buttonStop = document.querySelector(this.Selectors.buttonStop)
+
+        this.addEventListeners()
+    }
+
+    addEventListeners() {
+        this.button.addEventListener('click', () => {
+            if (this.hours.value !== "00" || this.minutes.value !== "00" || this.seconds.value !== "00") {
+                if (!this.hours.disabled && !this.minutes.disabled && !this.seconds.disabled) {
+                    this.startTimer();
+                }
+            }
+        })
+
+        this.buttonStop.addEventListener('click', () => {
+            this.stopTimer();
+        })
+
+        this.seconds.addEventListener('keyup', () => {
+            this.seconds.value > 0 ? this.sec = this.seconds.value : this.sec = this.seconds.value.replace("-", "")
+        })
+        this.minutes.addEventListener('keyup', () => {
+
+            this.minutes.value > 0 ? this.min = this.minutes.value : this.min = this.minutes.value.replace("-", "")
+        })
+        this.hours.addEventListener('keyup', () => {
+
+            this.hr = this.hours.value;
+
+        })
+    }
+
+    startTimer() {
+        if (this.hr > 0 || this.min > 0 || this.sec > 0) {
+            this.hr = parseInt(this.hr);
+            this.min = parseInt(this.min);
+            this.sec = parseInt(this.sec);
+        }
+        if (this.sec > 60) {
+            this.sec = 59
+        }
+        if (this.min > 60) {
+            this.min = 59
+        }
+        if (this.hr > 100) {
+            this.hr = 99
+        }
+        this.sec < 10 ? this.seconds.value = "0" + this.sec : this.seconds.value = this.sec
+        this.min < 10 ? this.minutes.value = "0" + this.min : this.minutes.value = this.min
+        this.hr < 10 ? this.hours.value = "0" + this.hr : this.hours.value = this.hr
+
+        this.hours.disabled = "true";
+        this.minutes.disabled = "true";
+        this.seconds.disabled = "true";
+
+        this.countSeconds()
+        this.countMinutes();
+        this.countHours();
+
+    }
+
+    stopTimer() {
+        this.hours.removeAttribute('disabled');
+        this.minutes.removeAttribute('disabled');
+        this.seconds.removeAttribute('disabled');
+        clearInterval(this.secCount)
+        clearInterval(this.minCount)
+        clearInterval(this.hrCount)
+
+    }
+
+    countSeconds() {
+        this.secCount = setInterval(() => {
+            //SEKUNDY
+            this.sec < 10 ? this.seconds.value = "0" + this.sec : this.seconds.value = this.sec
+            if (this.sec > 0) {
+                this.sec--
+            } else if (this.min > 0 && this.sec === 0) {
+                this.sec = 59;
+            }
+        }, 1000)
+    }
+
+    countMinutes() {
+        this.minCount = setInterval(() => {
+            // MINUTY
+            this.min < 10 ? this.minutes.value = "0" + this.min : this.minutes.value = this.min
+            if (this.min > 0 && this.sec === 59) {
+                this.min--
+            }
+        }, 1000)
+    }
+
+    countHours() {
+        this.hrCount = setInterval(() => {
+            // GODZINY
+            this.hr < 10 ? this.hours.value = "0" + this.hr : this.hours.value = this.hr
+            if (this.hr > 0 && this.min === 0 && this.sec === 0) {
+                this.hr--
+                this.min = 59
+                this.sec = 59
+            }
+        }, 1000)
+    }
+}
